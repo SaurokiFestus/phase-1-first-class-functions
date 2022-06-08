5f595bb2021fa9e73b1d86b26ffca0c3ca7a0e48
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-06-08 15:41:28 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback){
+    return callback()
+}
+
+function returnsANamedFunction(){
+return function named(){
+    return;
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return () => {}
+}
